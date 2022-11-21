dad180ed77842e5ceffc91601ba4fa18426683b5
alae-herrak <alaeherrak@gmail.com>
2022-11-21 12:38:37 +0100

    updated the handleLike logic

--- handleLike.php
+++ handleLike.php
@@ -4,14 +4,26 @@ if (
     isset($_SESSION['userID']) &&
     isset($_GET['postID'])
 ) {
+    //check if user already liked the post
     $postID = $_GET['postID'];
     require './db.php';
-    $stmt = $db->prepare('SELECT * FROM post WHERE postID=?');
-    $stmt->execute([$postID]);
-    $post = $stmt->fetch(PDO::FETCH_OBJ);
-    $stmt = $db->prepare('UPDATE post SET likesCount = ? WHERE postID = ?');
-    $stmt->execute([
-        $post->likesCount + 1,
-        $postID
-    ]);
+
+    $stmt = $db->prepare('SELECT * FROM likes WHERE postID = ? AND userID = ?');
+    $stmt->execute([$postID, $_SESSION['userID']]);
+    $liked = $stmt->fetch(PDO::FETCH_OBJ);
+
+    if ($liked == false) {
+        //add to post like count
+        $stmt = $db->prepare('SELECT * FROM post WHERE postID=?');
+        $stmt->execute([$postID]);
+        $post = $stmt->fetch(PDO::FETCH_OBJ);
+        $stmt = $db->prepare('UPDATE post SET likesCount = ? WHERE postID = ?');
+        $stmt->execute([
+            $post->likesCount + 1,
+            $postID
+        ]);
+        //add to likes table
+        $stmt = $db->prepare('INSERT INTO likes (postID, userID) VALUES (?, ?)');
+        $stmt->execute([$postID, $_SESSION['userID']]);
+    }
 }
